7ac73d6a1370c8cb7067fa2d027ec010551d5f0d
Aarti98765 <aartiahireengit@gmail.com>
2020-04-11 12:34:26 +0530

    ADDED:DAO for US Census data

--- src/main/java/censusanalyser/CensusDao.java
+++ src/main/java/censusanalyser/CensusDao.java
@@ -12,6 +12,14 @@ public class CensusDao {
     public int tin;
     public int SrNo;
     public String stateName;
+    public String StateId;
+    public String State;
+    public int Population;
+    public int HousingUnit;
+    public float TotalArea;
+    public float WaterArea;
+    public float LandArea;
+    public float PopulationDensity;
 
     public CensusDao(IndiaCensusCSV csvStatesCensus) {
         this.stateCode = csvStatesCensus.stateCode;
@@ -25,6 +33,17 @@ public class CensusDao {
         this.tin = csvStatesCensus.tin;
         this.stateName = csvStatesCensus.stateName ;
         this.stateCode = csvStatesCensus. stateCode;
+    }
+
+    public void CensusDAO(UsStateCSV csvUsCensus){
+        this.StateId = csvUsCensus.stateId;
+        this.State = csvUsCensus.state;
+        this.Population = csvUsCensus.population;
+        this.HousingUnit = csvUsCensus.housingUnits;
+        this.TotalArea = csvUsCensus.totalArea;
+        this.WaterArea = csvUsCensus.waterArea;
+        this.LandArea= csvUsCensus.landArea;
+        this.PopulationDensity = csvUsCensus.populationDensity;
     }
 }
 
